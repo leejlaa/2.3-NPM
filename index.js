f62b17bbc6fb427d4ce5542966b572700f90f62d
leejlaa <lejladoric00@gmail.com>
2023-09-15 13:26:33 +0200

    importing superhero module

---
+++ index.js
@@ -0,0 +1,9 @@
+import superheroes from "superheroes";
+import all from "superheroes";
+import random from "superheroes";
+
+var allNames = superheroes.all;
+var name = superheroes.random();
+
+console.log("I'm a "+name+"!");
+
